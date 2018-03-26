6a38b60fbc68169c651b5c589e969c8e207bee47
cai.chen <cai.chen@sap.com>
2018-03-26 18:18:50 +0800

    1. add new jar 2. refract the groovy script

--- GroovyBasic/src/tools/cpi/util/ControlGenerator.groovy
+++ GroovyBasic/src/tools/cpi/util/ControlGenerator.groovy
@@ -14,14 +14,67 @@ The methods available are:
 	   public void setProperty(java.lang.String name, java.lang.Object value)
  */
 import com.sap.gateway.ip.core.customdev.util.Message;
-
-import sap.maco.edifact.segment.UNASegement
+import sap.maco.edifact.message.EdifactMessageStructure;
+import sap.maco.edifact.segment.UNASegement;
+import sap.maco.edifact.dataelement.basic.ComplexDataElement;
+import sap.maco.edifact.dataelement.basic.DataElement;
+import sap.maco.edifact.segment.Segment;
+import sap.maco.edifact.segment.SegmentGroup;
+import sap.maco.edifact.util.LoadMigHelper;
+import sap.maco.edifact.factory.EdifactMessageFactory;
 
 import java.util.HashMap;
 
 def processData(){
+	def messageName = "contrl"
+	EdifactMessageStructure contrlMessage = EdifactMessageFactory.loadEdiFactMessageMig(messageName)
 	
-	println UNASegement.print();
+	def UNBSegment = this.getUNBSegment(contrlMessage);
+	
+	
+	if(UNBSegment.getSegmentStructure() != null){
+		UNBSegment.getSegmentStructure().getComplexDataElement("S001").getDataElement("0001").setValue("UNOC")
+		UNBSegment.getSegmentStructure().getComplexDataElement("S001").getDataElement("0002").setValue("3")
+		UNBSegment.getSegmentStructure().getComplexDataElement("S002").getDataElement("0004").setValue("ReceiverXYZ")
+		UNBSegment.getSegmentStructure().getComplexDataElement("S002").getDataElement("0007").setValue("14")
+		UNBSegment.getSegmentStructure().getComplexDataElement("S003").getDataElement("0010").setValue("SENDERABC")
+		UNBSegment.getSegmentStructure().getComplexDataElement("S003").getDataElement("0007").setValue("14")
+		UNBSegment.getSegmentStructure().getComplexDataElement("S004").getDataElement("0017").setValue("180307")
+		UNBSegment.getSegmentStructure().getComplexDataElement("S004").getDataElement("0019").setValue("0541")
+	}
+	
+	println(contrlMessage.printEdiFactMessage())
+	
+}
+
+def Segment getUNBSegment(EdifactMessageStructure message){
+	def UNBSegmentName = "UNB"
+	
+	return message.getSegment("UNB")
+}
+
+def Segment getUNHSegment(EdifactMessageStructure message){
+	def UNHSegmentName = "UNH"
+	
+	return message.getSegment(UNHSegmentName)
+}
+
+def Segment getUCISegment(EdifactMessageStructure message){
+	def UCISegmentName = "UCI"
+	
+	return message.getSegment(UCISegmentName)
+}
+
+def Segment getUNTSegment(EdifactMessageStructure message){
+	def UNTSegmentName = "UNT"
+	
+	return message.getSegment(UNTSegmentName)
+}
+
+def Segment getUNZSegment(EdifactMessageStructure message){
+	def UNZSegmentName = "UNZ"
+	
+	return message.getSegment(UNZSegmentName)
 }
 
 processData()
